6920e12489d052e7ecb228d82c44d65dd62a124b
Liliya <liliya.bevcuktratata@gmail.com>
2020-05-17 22:08:06 +0300

    structure

--- script.py
+++ script.py
@@ -1,14 +1,10 @@
-from data_ocean.services.ruo_service import RuoConverter
-from data_ocean.services.ratu_service import RatuConverter
-from data_ocean.services.rfop_service import RfopConverter
-from data_ocean.services.kved_service import KvedConverter
-from data_ocean.services.koatuu_service import KoatuuConverter
+from location_register.converter.koatuu import KoatuuConverter
 
 KoatuuConverter().download_file()
-KvedConverter().download_file()
-RatuConverter().download_file()
-RuoConverter().download_file()
-RfopConverter().download_file()
+# KvedConverter().download_file()
+# RatuConverter().download_file()
+# RuoConverter().download_file()
+# RfopConverter().download_file()
 
 # KvedConverter().process()
 # RatuConverter().process()
